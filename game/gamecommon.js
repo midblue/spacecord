405534f08f062af1461671141a60e1428eab2cdd
Xander Madsen <xander.madsen@gmail.com>
2021-03-10 16:42:22 -0500

    Add vector gravity stuff blahblah

--- game/gamecommon.js
+++ game/gamecommon.js
@@ -1,3 +1,6 @@
+
+const { distance, getUnitVector } = require(`../common`)
+
 module.exports = {
   log(type = `Core`, ...text) {
     console.log(`> ${type.padEnd(15, ` `)} |`, ...text)
@@ -27,4 +30,19 @@ module.exports = {
   getTrainingXp(minigameScore, skillLevel) {
     return 235 + 5 * skillLevel * minigameScore // yea some magic numbers sorry eslint
   },
+
+  getGravityForceVector(thisBody, thatBody) {
+    if (!thisBody || !thatBody || !thisBody.mass || !thatBody.mass)
+      return [0, 0]
+
+    m1 = thisBody.mass
+    m2 = thatBody.mass
+
+    r = distance(...thisBody.location, ...thatBody.location)
+    let G = GRAVITATIONAL_CONSTANT
+    gravityForce = -G * m1 * m2 / (r ** 2)
+    vectorToThisBody = getUnitVector(thisBody, thatBody)
+
+    return vectorToThisBody.map((i) => i * gravityForce)
+  }
 }

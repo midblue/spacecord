5db45144172df84ce05d0fc7289adb5720b39c75
Xander Madsen <xander.madsen@gmail.com>
2021-03-16 09:56:51 -0400

    Got gravity tests all working

--- test/testMotion.js
+++ test/testMotion.js
@@ -84,7 +84,7 @@ describe(`Gravity`, async () => {
     }, testPlanet)).not.to.deep.equal([0, 0])
   })
 
-  it(`should have a vector pointing from thisBody to thatBody between two objects in different locations`, async () => {
+  it(`should have a vector pointing from thatBody to thisBody between two objects in horizontal line`, async () => {
     const game = require(`../game/manager`)
 
     const testPlanet = {
@@ -117,7 +117,77 @@ describe(`Gravity`, async () => {
 
   })
 
+  it(`should have the correct vector pointing from thatBody to thisBody between two objects in vertical line`, async () => {
+    const game = require(`../game/manager`)
+
+    const testPlanet = {
+      name: `Test Planet 1`,
+      location: [0, 2],
+      color: `white`,
+      mass: 100000
+    }
+    game.planets.push(testPlanet)
+
+    const testShip = await addShip({
+      name: `Test Ship 1`,
+      location: [0, 0],
+      debugMass: 1000,
+      bearing: [0, 0]
+    })
+
+    forceVectorOnShip = getGravityForceVectorOnThisBodyDueToThatBody(
+      {
+        ...testShip,
+        mass: testShip.getTotalMass(),
+      },
+      testPlanet
+    )
+
+    expectedForceVector = [0, GRAVITATIONAL_CONSTANT * testPlanet.mass * testShip.getTotalMass() / (2 ** 2)]
+
+    assert(almostEqual(forceVectorOnShip[0], expectedForceVector[0], GRAV_TOLERANCE))
+    assert(almostEqual(forceVectorOnShip[1], expectedForceVector[1], GRAV_TOLERANCE))
+
+  })
+
+  it(`should have the correct vector pointing from thatBody to thisBody between two objects on 45-degree diagonal line`, async () => {
+    const game = require(`../game/manager`)
+
+    const testPlanet = {
+      name: `Test Planet 1`,
+      location: [2, 2],
+      color: `white`,
+      mass: 100000
+    }
+    game.planets.push(testPlanet)
+
+    const testShip = await addShip({
+      name: `Test Ship 1`,
+      location: [0, 0],
+      debugMass: 1000,
+      bearing: [0, 0]
+    })
+
+    forceVectorOnShip = getGravityForceVectorOnThisBodyDueToThatBody(
+      {
+        ...testShip,
+        mass: testShip.getTotalMass(),
+      },
+      testPlanet
+    )
+
+    expectedMagnitude = GRAVITATIONAL_CONSTANT * testPlanet.mass * testShip.getTotalMass() / ((2 * Math.sqrt(2)) ** 2)
+    expectedForceVector = [expectedMagnitude / Math.sqrt(2), expectedMagnitude / Math.sqrt(2)]
+    console.log(`calculated:`, forceVectorOnShip)
+    console.log(`expected:`, expectedForceVector)
+    assert(almostEqual(forceVectorOnShip[0], expectedForceVector[0], GRAV_TOLERANCE))
+    assert(almostEqual(forceVectorOnShip[1], expectedForceVector[1], GRAV_TOLERANCE))
+
+  })
+
+
 })
+
 
 before(() => {
   console.log = (...args) => { outputToWriteToFile.push(args.map((a) => typeof a === `object` ? JSON.stringify(a, null, 2) : a)) }

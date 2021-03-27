675e53d1a2c3ce0ed1450553cbb628ecfeb4939c
Jasper Stephenson <jasperstephenson@gmail.com>
2021-03-27 13:39:26 +0100

    more tests

--- test/testShipCargo.js
+++ test/testShipCargo.js
@@ -8,34 +8,74 @@ const models = require(`../db/mongo/models`)
 const chaiAlmost = require(`chai-almost`)
 chai.use(chaiAlmost())
 const utils = require(`./tools/utils`)
+const game = require(`../game/manager`)
 
 let outputToWriteToFile = []
 
 describe(`Ship Cargo`, () => {
-    it(`should be possible to add cargo to a ship`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(`should be possible to add cargo to a ship`, async () => {
+    await utils.addShip({})
+    const guild = game.guilds[0]
+    const fuel = guild.ship.cargo.find((c) => c.cargoType === `fuel`)
+    const initial = fuel.amount
+    guild.ship.addCargo(`fuel`, 1, 0)
+    const final = fuel.amount
+    chai.expect(initial).to.equal(final - 1)
+  })
 
-    it(`should be possible to remove cargo from a ship`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(`should be possible to remove cargo from a ship`, async () => {
+    const guild = game.guilds[0]
+    const fuel = guild.ship.cargo.find((c) => c.cargoType === `fuel`)
+    const initial = fuel.amount
+    guild.ship.removeCargo(`fuel`, 1, 0)
+    const final = fuel.amount
+    chai.expect(initial).to.equal(final + 1)
+  })
 
-    it(`should not be possible to remove more cargo from a ship than it is currently carrying`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(`should not be possible to remove more cargo from a ship than it is currently carrying`, async () => {
+    const guild = game.guilds[0]
+    const fuel = guild.ship.cargo.find((c) => c.cargoType === `fuel`)
+    fuel.amount = 1
+    guild.ship.removeCargo(`fuel`, 2, 0)
+    const final = fuel.amount
+    chai.expect(final).to.equal(0)
+  })
 
-    before(() => {
-        console.log = (...args) => {
-            outputToWriteToFile.push(
-                args.map((a) => (typeof a === `object` ? JSON.stringify(a, null, 2) : a)),
-            )
-        }
-    })
+  it(`should spawn a cache on cargo jettison`, async () => {
+    const guild = game.guilds[0]
+    const fuel = guild.ship.cargo.find((c) => c.cargoType === `fuel`)
+    fuel.amount = 1
+    guild.ship.jettison(`fuel`, 1, `Hello`)
+    assert(game.caches[0])
+    chai.expect(game.caches[0].message).to.equal(`Hello`)
+  })
 
-    after(() => {
-        fs.writeFileSync(
-            path.resolve(`./`, `test/output`, `shipCargo.txt`),
-            outputToWriteToFile.join(`\n`),
-        )
-    })
+  before(async () => {
+    try {
+      await mongoose.connection.collection(`guilds`).drop()
+    } catch (e) {}
+    try {
+      await mongoose.connection.collection(`ships`).drop()
+    } catch (e) {}
+    try {
+      await mongoose.connection.collection(`caches`).drop()
+    } catch (e) {}
+    game.guilds = []
+    game.caches = []
+
+    console.log = (...args) => {
+      outputToWriteToFile.push(
+        args.map((a) =>
+          typeof a === `object` ? JSON.stringify(a, null, 2) : a,
+        ),
+      )
+    }
+  })
+
+  after(() => {
+    fs.writeFileSync(
+      path.resolve(`./`, `test/output`, `shipCargo.txt`),
+      outputToWriteToFile.join(`\n`),
+    )
+  })
 })
--- test/testShipCombat.js
+++ test/testShipCombat.js
@@ -8,51 +8,85 @@ const models = require(`../db/mongo/models`)
 const chaiAlmost = require(`chai-almost`)
 chai.use(chaiAlmost())
 const utils = require(`./tools/utils`)
+const game = require(`../game/manager`)
+const equipment = require(`../game/basics/equipment/equipment`)
 
 let outputToWriteToFile = []
 
 describe(`Ship Combat`, () => {
-    it(`should use ship armor first to mitigate damage taken`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(`should be able to hit a ship within range`, async () => {
+    const ship1 = game.guilds[0].ship
+    const ship2 = game.guilds[1].ship
+  })
 
-    it(`should take equipment damage on being hit by an attack`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(`should always miss a ship outside of range`, async () => {
+    const ship1 = game.guilds[0].ship
+    const ship2 = game.guilds[1].ship
+  })
 
-    it(`should deal cascading damage to other equipment on destroying the first piece`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(`should take equipment damage on being hit by an attack`, async () => {
+    const ship1 = game.guilds[0].ship
+    const ship2 = game.guilds[1].ship
+  })
 
-    it(`should take less damage to ship armor than other equipment`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(`should take the right amount of damage on being hit by an attack`)
 
-    it(`should be possible to target a specific piece of equipment on a ship`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(
+    `should deal cascading damage to other equipment on destroying the first piece`,
+  )
 
-    it(`should destroy the ship on its reaching 0 hp`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(`should use ship armor first to mitigate damage taken`)
 
-    it(`should spawn an attack remnant on each attack`, async () => {
-        assert(false, `Tony Kong <(B{|)`)
-    })
+  it(`should take less damage to ship armor than other equipment`)
 
-    before(() => {
-        console.log = (...args) => {
-            outputToWriteToFile.push(
-                args.map((a) => (typeof a === `object` ? JSON.stringify(a, null, 2) : a)),
-            )
-        }
-    })
+  it(`should be possible to target a specific piece of equipment on a ship`)
 
-    after(() => {
-        fs.writeFileSync(
-            path.resolve(`./`, `test/output`, `shipCombat.txt`),
-            outputToWriteToFile.join(`\n`),
-        )
+  it(`should destroy the ship on its reaching 0 hp`)
+
+  it(`should jettison all of the ship's cargo on death, scaled by a percentage`)
+
+  it(`should spawn an attack remnant on each attack`)
+
+  before(async () => {
+    try {
+      await mongoose.connection.collection(`guilds`).drop()
+    } catch (e) {}
+    try {
+      await mongoose.connection.collection(`ships`).drop()
+    } catch (e) {}
+    try {
+      await mongoose.connection.collection(`users`).drop()
+    } catch (e) {}
+    game.guilds = []
+    game.planets = []
+    const ship1 = await utils.addShip({
+      location: [1, 1],
+      status: { docked: false, dead: false },
     })
+    const ship2 = await utils.addShip({
+      location: [1.00001, 1.00001],
+      status: { docked: false, dead: false },
+    })
+    ship1.equipment.find((eq) => eq.equipmentType === `weapon`).list = [
+      equipment.weapon.debugAlwaysHit1,
+    ]
+    ship2.equipment.find((eq) => eq.equipmentType === `weapon`).list = [
+      equipment.weapon.debugAlwaysMiss,
+    ]
+
+    console.log = (...args) => {
+      outputToWriteToFile.push(
+        args.map((a) =>
+          typeof a === `object` ? JSON.stringify(a, null, 2) : a,
+        ),
+      )
+    }
+  })
+
+  after(() => {
+    fs.writeFileSync(
+      path.resolve(`./`, `test/output`, `shipCombat.txt`),
+      outputToWriteToFile.join(`\n`),
+    )
+  })
 })
-
